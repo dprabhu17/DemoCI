ee385dd60b651e87dec7f38d34ca179c7dac948c
Prabhu Durai <p.durai@lakeba.com>
2018-11-16 18:31:52 +0530

    Demo source code for CI and CD

---
+++ Pods/Target Support Files/Pods-DemoCITests/Pods-DemoCITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoCITests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoCITests
+@end
---
+++ Pods/Target Support Files/Pods-DemoCIUITests/Pods-DemoCIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoCIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoCIUITests
+@end
